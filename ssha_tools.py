bb92733c590e300e5835d72f0dd9d4c316b2376e
Drew Nase <ajn212@lehigh.edu>
2014-03-25 10:24:47 -0400

    took a few cycles off hash encoding

--- ssha_tools.py
+++ ssha_tools.py
@@ -2,7 +2,7 @@ import base64, hashlib
 
 def _get_salt(rawhash):
     #strip off {SSHA} if it's there
-    if rawhash[0:6] == '{SSHA}' or rawhash[0:6] == '{ssha}':
+    if rawhash[0] == '{':
         myhash = rawhash[6:]
     else:
         myhash = rawhash
@@ -14,6 +14,5 @@ def _hash_string(my_string, salt):
     return base64.b64encode(hashlib.sha1(my_string + salt).digest() + salt)
 
 def try_pw(my_string, my_hash):
-    my_salt = _get_salt(my_hash)
-    th = _hash_string(my_string, my_salt)
+    th = _hash_string(my_string, _get_salt(my_hash))
     return (my_hash == th or my_hash == ('{SSHA}' + th) or my_hash == ('{ssha}' + th)) 
